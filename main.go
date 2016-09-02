bc198d8e582b80e051258218fde658ec483d0d6b
Odd E. Ebbesen <odd.ebbesen@wirelesscar.com>
2016-09-02 11:23:20 +0200

    Added more flags/options

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 const (
 	VERSION string = "2016-09-02"
+)
 
 func main() {
 	app := cli.NewApp()
@@ -14,7 +15,7 @@ func main() {
 	app.Version = VERSION
 	app.Authors = []cli.Author{
 		cli.Author{
-			Name: "Odd E. Ebbesen",
+			Name:  "Odd E. Ebbesen",
 			Email: "odd.ebbesen@wirelesscar.com",
 		},
 	}
@@ -22,12 +23,36 @@ func main() {
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name: "url, u",
+			Name:  "url, u",
 			Usage: "Full URL (with port) to Influx endpoint",
 		},
 		cli.StringFlag{
-			Name: "db",
+			Name:  "db",
 			Usage: "Name of database to write to",
+		},
+		cli.StringFlag{
+			Name:  "hostname-prefix, h",
+			Usage: "Prefix for generated hostnames",
+		},
+		cli.UintFlag{
+			Name:  "num-hosts, n",
+			Usage: "Number of hosts to simulate traffic from",
+		},
+		cli.Float64Flag{
+			Name:  "interval, i",
+			Usage: "How long (in seconds, fractions allowed) between sending metrics",
+		},
+		cli.Float64Flag{
+			Name:  "timeout, t",
+			Usage: "How long in seconds (fractions allowed) to run the test",
+		},
+		cli.StringFlag{
+			Name:  "measurement, m",
+			Usage: "The Influx unit to report on, e.g. 'cpu'",
+		},
+		cli.StringFlag{
+			Name:  "field, f",
+			Usage: "The field to set the generated value on, e.g. 'load'",
 		},
 	}
 }
